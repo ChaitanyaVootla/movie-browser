af2e33bde3fdc67fb7d476e8931d165c09b4939d
Vootla Chaitanya <speedblaze@gmail.com>
2021-11-02 15:11:23 +0530

    css fix

--- src/components/Common/movieCard.vue
+++ src/components/Common/movieCard.vue
@@ -276,8 +276,9 @@
     }
     .popover-bg-image {
         position: absolute;
-        object-fit: contain;
-        max-width: 100%;
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
         overflow: hidden;
         z-index: -1;
         opacity: 0.2;
