17a7b9acc86d1b49070ddc09407d1ffdb41d755c
Chaitanya Vootla <speedblaze@gmail.com>
2022-09-07 17:53:06 +0530

    Update mongo index

--- server/db/setup.ts
+++ server/db/setup.ts
@@ -3,11 +3,9 @@ import { Db } from "mongodb";
 const setupDb = (db: Db) => {
     // movie
     db.collection('movie').createIndex({id:1}, {unique: true});
-    db.collection('movie').createIndex({imdb_id:1}, {unique: true, sparse: true});
 
     // tv
     db.collection('tv').createIndex({id:1}, {unique: true});
-    db.collection('tv').createIndex({imdb_id:1}, {unique: true, sparse: true});
 }
 
 export { setupDb };
