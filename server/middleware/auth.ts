820ccb9b61481040fa4383a070505c260861af48
Chaitanya Vootla <speedblaze@gmail.com>
2023-12-21 19:26:52 +0530

    test

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,8 +1,6 @@
-import { getToken, getServerSession } from '#auth'
+import { getToken } from '#auth'
 
 export default eventHandler(async (event) => {
   const userData = await getToken({ event })
-  const session = await getServerSession(event);
-  console.log(session, userData)
   event.context.userData = userData;
 })
