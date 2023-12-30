578ae72754abce54abdbab0cac045b2c3d88d8c1
Sahithi Yarlagadda <sahithiyarlagadda79@gmail.com>
2023-12-30 22:34:22 +0530

    scoller update

--- components/Scroller.vue
+++ components/Scroller.vue
@@ -3,45 +3,110 @@
         <div class="title text-base md:text-lg font-light -mb-2 md:mb-0 md:ml-14">
             {{ title }}
         </div>
-        <v-slide-group show-arrows="desktop">
-            <v-slide-group-item v-for="item in (items || Array(10))">
-                <slot :item="item">
-                    <PosterCard :item="item" :pending="pending" class="mr-2 md:pr-6"/>
-                </slot>
-            </v-slide-group-item>
-        </v-slide-group>
+        <div class="flex w-full h-full">
+            <div class="h-auto w-14 flex items-center justify-center cursor-pointer max-md:hidden" v-on:click="slideLeft">
+                <div>
+                    <v-icon icon="mdi-chevron-left" color="#aaa"></v-icon>
+                </div>
+            </div>
+            <div class="flex gap-2 md:gap-4 overflow-y-auto w-full" :id="`scroll-bar-${uuid}`">
+                <div v-for="item in (items || Array(10))">
+                    <slot :item="item">
+                        <PosterCard :item="item" :pending="pending" class="mr-2 md:pr-6"
+                            :class="isSliding ? 'pointer-events-none' : ''"/>
+                    </slot>
+                </div>
+            </div>
+            <div class="h-auto w-14 flex items-center justify-center cursor-pointer max-md:hidden" v-on:click="slideRight">
+                <v-icon icon="mdi-chevron-right" color="#aaa"></v-icon>
+            </div>
+        </div>
     </div>
 </template>
 
-<script lang="ts">
-export default {
-    name: "Scroller",
-    props: {
-        items: {
-            type: Object,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        pending: {
-            type: Boolean,
-            required: true
-        }
+<script setup lang="ts">
+import { v4 as uuidv4 } from 'uuid';
+
+defineProps({
+    items: {
+        type: Object,
+        required: true
     },
-}
+    title: {
+        type: String,
+        required: true
+    },
+    pending: {
+        type: Boolean,
+        required: true
+    }
+});
+const identifier = uuidv4();
+const uuid = ref(identifier);
+let isSliding = ref(false);
+
+onMounted(() => {
+    setTimeout(() => {
+        const slider: any = document.querySelector(`#scroll-bar-${uuid.value}`);
+
+        if (slider) {
+            let isDown = false;
+            let startX: any;
+            let scrollLeft: any;
+
+            const startDragging = (e: any) => {
+                isDown = true;
+                startX = e.pageX - slider.offsetLeft;
+                scrollLeft = slider.scrollLeft;
+                e.preventDefault();
+            };
+
+            const stopDragging = () => {
+                if (!isDown) return;
+                isDown = false;
+                isSliding.value = false;
+            };
+
+            const whileDragging = (e: any) => {
+                if (!isDown) return;
+                isSliding.value = true;
+                const x = e.pageX - slider.offsetLeft;
+                const walk = x - startX;
+
+                requestAnimationFrame(() => {
+                    slider.scrollLeft = scrollLeft - walk;
+                });
+                e.preventDefault();
+            };
+
+            slider.addEventListener('mousedown', startDragging);
+            slider.addEventListener('mouseleave', stopDragging);
+            slider.addEventListener('mouseup', stopDragging);
+            slider.addEventListener('mousemove', whileDragging);
+        }
+    })
+});
+
+const slideLeft = () => {
+    const slider = document.querySelector(`#scroll-bar-${uuid.value}`) as HTMLElement;
+    slider.classList.add('scroll-smooth');
+    slider.scrollLeft -= slider.clientWidth;
+    slider.classList.remove('scroll-smooth');
+};
+
+const slideRight = () => {
+    const slider: any = document.querySelector(`#scroll-bar-${uuid.value}`);
+    slider.classList.add('scroll-smooth');
+    slider.scrollLeft += slider.clientWidth;
+    slider.classList.remove('scroll-smooth');
+};
 </script>
 
 <style scoped lang="less">
 ::-webkit-scrollbar {
     display: none;
 }
-.scroller {
+.scroller-container {
     display: flex;
-    overflow-x: auto;
-    :deep(.card) {
-        margin-right: 2rem;
-    }
 }
 </style>
