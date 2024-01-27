2e4fd001b85e2294cae1ba6d0a854c4f7e277f32
Sahithi Yarlagadda <sahithiyarlagadda79@gmail.com>
2024-01-27 10:08:14 +0530

    trending retry

--- server/api/trending/trendingTmdb.ts
+++ server/api/trending/trendingTmdb.ts
@@ -1,11 +1,19 @@
 export default defineEventHandler(async (event) => {
     try {
       const [{ results: allItems }, { results: movies }, { results: tv }, { results: streamingNow}] = (await Promise.all([
-        $fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.TMDB_API_KEY}`),
-        $fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}`),
-        $fetch(`https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.TMDB_API_KEY}`),
+        $fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.TMDB_API_KEY}`, {
+          retry: 5
+        }),
+        $fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}`, {
+          retry: 5
+        }),
+        $fetch(`https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.TMDB_API_KEY}`, {
+          retry: 5
+        }),
         $fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY
-          }&watch_region=IN&with_watch_monetization_types=free|flatrate|buy|rent|ads`),
+          }&watch_region=IN&with_watch_monetization_types=free|flatrate|buy|rent|ads`, {
+            retry: 5
+        }),
       ])) as any[];
       return {
         allItems,
