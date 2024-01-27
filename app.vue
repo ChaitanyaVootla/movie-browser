e0adf51ddb0b559cc88d5d324fb62778a698c9df
Sahithi Yarlagadda <sahithiyarlagadda79@gmail.com>
2024-01-27 11:16:06 +0530

    footer

--- app.vue
+++ app.vue
@@ -4,25 +4,33 @@
     <v-app>
       <NavBar/>
       <NuxtPage :page-key="route => route.path" class="mb-10" :class="$vuetify.display.mobile?'pb-10':''" />
-      <v-footer class="!p-0">
-        <div class="flex gap-10 text-sm justify-center py-5 w-full bg-neutral-900 text-neutral-400">
+      <div class="h-4 absolute bottom-0 w-full">
+        <div class="flex gap-10 text-sm justify-center py-3 w-full bg-neutral-900 text-neutral-400">
           <div class="flex items-baseline">
             Created by
-            <NuxtLink href="https://vootlachaitanya.com" noreferrer noopener target="_blank" class="ml-2 underline underline-offset-4">
+            <NuxtLink href="https://vootlachaitanya.com" noreferrer noopener target="_blank"
+              class="ml-2 underline underline-offset-4">
               Chaitanya Vootla
             </NuxtLink>
           </div>
           <div class="flex items-baseline">
+            Github Repo
+            <v-btn prepend-icon="mdi-github" href="https://github.com/ChaitanyaVootla/movie-browser/tree/nuxt"
+              noreferrer noopener target="_blank" size="x-small" class="ml-2" color="#333">
+              ChaitanyaVootla/movie-browser
+            </v-btn>
+          </div>
+          <div class="flex items-baseline">
             Powered by
             <NuxtLink href="https://developer.themoviedb.org/reference/intro/getting-started" noreferrer noopener target="_blank">
-              <NuxtImg src="images/tmdb.svg" class="h-4 ml-2"></NuxtImg>
+              <NuxtImg src="images/tmdb.svg" class="h-3 ml-2"></NuxtImg>
             </NuxtLink>
           </div>
           <div class="flex items-baseline">
             Built with <NuxtImg src="images/nuxt.svg" class="h-4 ml-2"></NuxtImg>
           </div>
         </div>
-      </v-footer>
+      </div>
     </v-app>
   </NuxtLayout>
 </template>
