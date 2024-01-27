9feb084465581df4b5b8575ca3a111a7a12fbf70
Sahithi Yarlagadda <sahithiyarlagadda79@gmail.com>
2024-01-27 10:48:50 +0530

    footer

--- app.vue
+++ app.vue
@@ -4,6 +4,25 @@
     <v-app>
       <NavBar/>
       <NuxtPage :page-key="route => route.path" class="mb-10" :class="$vuetify.display.mobile?'pb-10':''" />
+      <v-footer class="!p-0">
+        <div class="flex gap-10 text-sm justify-center py-5 w-full bg-neutral-900 text-neutral-400">
+          <div class="flex items-baseline">
+            Created by
+            <NuxtLink href="https://vootlachaitanya.com" noreferrer noopener target="_blank" class="ml-2 underline underline-offset-4">
+              Chaitanya Vootla
+            </NuxtLink>
+          </div>
+          <div class="flex items-baseline">
+            Powered by
+            <NuxtLink href="https://developer.themoviedb.org/reference/intro/getting-started" noreferrer noopener target="_blank">
+              <NuxtImg src="images/tmdb.svg" class="h-4 ml-2"></NuxtImg>
+            </NuxtLink>
+          </div>
+          <div class="flex items-baseline">
+            Built with <NuxtImg src="images/nuxt.svg" class="h-4 ml-2"></NuxtImg>
+          </div>
+        </div>
+      </v-footer>
     </v-app>
   </NuxtLayout>
 </template>
--- server/api/trending/trendingTmdb.ts
+++ server/api/trending/trendingTmdb.ts
@@ -27,5 +27,4 @@ export default defineEventHandler(async (event) => {
         error: event.message,
       }
     }
-  });
-  
+});
