a950b401f801674c1da23ff72460b5d4f520b07b
Chaitanya vootla <vootlac@jfrog.com>
2024-02-01 12:13:31 +0530

    test

--- VectorDB/openai/server.py
+++ VectorDB/openai/server.py
@@ -7,6 +7,7 @@ import os
 import json
 import numpy as np
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
@@ -63,7 +64,18 @@ def perform_query(embedding, excludeIds, ratingCutoff, n_results):
         query_embeddings=embedding,
     )
 
+def log_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        duration = round(end_time - start_time, 2)
+        print(f"Endpoint: {request.path} took {duration} seconds to execute")
+        return result
+    return wrapper
+
 @app.route('/recommend', methods=['POST'])
+@log_time
 def recommend():
     body = request.json
     similarityMovieId = body.get('similarityMovieId')
@@ -72,11 +84,14 @@ def recommend():
     hideWatched = bool(body.get('hideWatched'))
     ratingCutoff = int(body.get('ratingCutoff', 0))
 
-    watchedMovieIds = json.load(open('watched.json', 'r'))
+    # watchedMovieIds = json.load(open('watched.json', 'r'))
+    watchedMovieIds = body.get('watchedIds') or []
     allEmbeddings = []
 
     if queryString:
+        start_time = time.time()
         allEmbeddings.append(openai.embeddings.create(input=queryString, model=MODEL_NAME).data[0].embedding)
+        print(f"openai.embeddings.create took {round(time.time() - start_time, 2)} seconds")
 
     if watchedQuery is not None and len(watchedMovieIds) > 0:
         watchedEmbedding = averageByIds(watchedMovieIds)
@@ -93,19 +108,36 @@ def recommend():
         return jsonify([])
 
     if len(allEmbeddings) > 1:
-        print(f"avegaring {len(allEmbeddings)} embeddings")
         embeddings_array = np.array(allEmbeddings)
         average_embedding = np.mean(embeddings_array, axis=0)
     else:
         average_embedding = allEmbeddings[0]
 
+    # Convert average_embedding to a list if it's a numpy array
+    if isinstance(average_embedding, np.ndarray):
+        average_embedding = average_embedding.tolist()
+
+    start_time = time.time()
     results = perform_query(average_embedding, hideWatched and watchedMovieIds or [0], ratingCutoff, 30)
+    print(f"chroma query took {round(time.time() - start_time, 2)} seconds")
 
     ids = [int(id) for id in results['ids'][0]]
     distances = results['distances'][0]
 
-    mongo_results = mongo_collection.find({'id': {'$in': ids}})
-    return jsonify(getMovieArray(mongo_results, ids, distances, watchedMovieIds))
+    mongo_results = mongo_collection.find({'id': {'$in': ids}}, {
+        'id': 1,
+        'title': 1,
+        'overview': 1,
+        'poster_path': 1,
+        'vote_average': 1,
+        'vote_count': 1,
+        'release_date': 1,
+        'backdrop_path': 1,
+        'popularity': 1,
+        'genres': 1,
+    }).sort([('popularity', -1)])
+    documents = list(mongo_results)
+    return jsonify(getMovieArray(documents, ids, distances, watchedMovieIds))
 
 
 def getMovieArray(dbMovies, ids, distances, watchedMovieIds):
--- server/api/movie/recommend.ts
+++ server/api/movie/recommend.ts
@@ -1,5 +1,19 @@
+import { JWT } from "next-auth/jwt";
+import { WatchedMovies } from "~/server/models";
+
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event);
+    let body = await readBody(event);
+
+    const userData = event.context.userData as JWT;
+    if (userData?.sub) {
+        const watched = await WatchedMovies.find({userId: userData?.sub}).select('movieId').sort({updatedAt: -1});
+        if (watched.length) {
+            body = {
+                ...body,
+                watchedIds: watched.map(({ movieId }) => movieId),
+            }
+        }
+    }
     const movies: any = await $fetch('http://127.0.0.1:5000/recommend', {
         method: 'POST',
         body,
