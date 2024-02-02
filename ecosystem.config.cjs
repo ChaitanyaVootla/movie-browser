4b195a0e739b4afc77f4fe46312d95097650ba83
Chaitanya vootla <vootlac@jfrog.com>
2024-02-02 22:24:27 +0530

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,11 @@ module.exports = {
   apps: [
     {
         name: "nuxt",
-        script: "cd VectorDB && export $(cat .env | xargs) && node .output/server/index.mjs",
+        script: "export $(cat .env | xargs) && node .output/server/index.mjs",
     },
     {
         name: "vector",
-        script: "source vector/bin/activate && python3 openai/server.py",
+        script: "cd VectorDB && source vector/bin/activate && python3 openai/server.py",
         max_memory_restart: "450M",
     },
   ],
