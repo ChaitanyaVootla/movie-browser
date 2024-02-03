714d6950f5aff1f28bd3ea2c66d4fb3944c92b58
Chaitanya vootla <vootlac@jfrog.com>
2024-02-03 15:45:31 +0530

    vector server fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
     },
     {
         name: "vector",
-        script: "cd VectorDB && source vector/bin/activate && python3 openai/server.py",
+        script: "cd VectorDB && source vector/bin/activate && python3 server.py",
         max_memory_restart: "450M",
     },
   ],
