187ef2e30c35493a9398202907c368ff58ccdb6c
Vootla Chaitanya <speedblaze@gmail.com>
2025-01-01 12:45:58 +0530

    basic sw caching

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -2,9 +2,13 @@
 /// <reference types="vite/client" />
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { clientsClaim } from 'workbox-core'
+import { CacheFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
 import { NavigationRoute, registerRoute } from 'workbox-routing'
 
 declare let self: ServiceWorkerGlobalScope
+const ONE_HOUR_SECS = 60 * 60
+const ONE_DAY_SECS = 24 * ONE_HOUR_SECS
 
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
@@ -21,6 +25,64 @@ registerRoute(new NavigationRoute(
   createHandlerBoundToURL('/'),
   { allowlist },
 ))
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/api/trending'),
+  new CacheFirst({
+    cacheName: 'trending-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: ONE_HOUR_SECS * 2,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/api/person'),
+  new CacheFirst({
+    cacheName: 'person-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: ONE_DAY_SECS,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  ({ url }) => {
+    const regex = /^\/api\/(movie|series)\/\d+$/
+    return regex.test(url.pathname)
+  },
+  new CacheFirst({
+    cacheName: 'movie-series-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: ONE_DAY_SECS,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  ({ url }) => {
+    const regex = /^\/api\/(movie|series)\/\d+\/recommend$/
+    return regex.test(url.pathname)
+  },
+  new CacheFirst({
+    cacheName: 'movie-series-secondary-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: ONE_DAY_SECS,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  }),
+);
 
 self.skipWaiting()
 clientsClaim()
