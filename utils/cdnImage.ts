17682b9db0c8a84a8d963e22aa67361d45b3fe47
Vootla Chaitanya <speedblaze@gmail.com>
2025-01-21 16:02:08 +0530

    cdn udpate

--- utils/cdnImage.ts
+++ utils/cdnImage.ts
@@ -2,9 +2,9 @@ export enum IMAGE_TYPE {
     POSTER = 'poster',
     LOGO = 'logo',
     BACKDROP = 'backdrop',
-    WIDE_CARD = 'wide_card',
+    WIDE_CARD = 'widePoster',
 }
 
 export const getCdnImage = (item: any, imageType: IMAGE_TYPE) => {
-    return `https://d2qifmj8erqnak.cloudfront.net/${item.title?'movie':'tv'}/${item.id}/${imageType}.avif`;
+    return `https://d2qifmj8erqnak.cloudfront.net/${item.title?'movie':'series'}/${item.id}/${imageType}.webp`;
 }
